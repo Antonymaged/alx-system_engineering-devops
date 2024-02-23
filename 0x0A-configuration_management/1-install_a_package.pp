ba15de2e352c551a5c67ee874b6a6440f1f6d64a
Antonymaged <magedantony1@gmaul.com>
2024-02-23 15:11:03 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
+#!/usr/bin/pup
 # instaling flask using pip by Puppet
 
-exec {'flask 2.1.0':
-  command  => 'usr/bin/pip install flask requests==2.1.0',
+package {'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
