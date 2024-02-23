a019a862cab3818c73f58efe84f98f48674b11e8
Antonymaged <magedantony1@gmaul.com>
2024-02-23 14:47:37 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #make a new file caled school contains the string I love Puppet
 file{'/tmp/school':
-ensure => file,
-owner => www-data,
-group => www-data,
-mode => 0744,
+ensure => 'file',
+owner => 'www-data',
+group => 'www-data',
+mode => '0744',
 content => "I love Puppet",
 }
